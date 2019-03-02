8f4e36444acdad7fc5fa120aeee2988c467da492
Shengda <dingshengda@gmail.com>
2019-03-01 23:16:49 -0800

    instrument management

--- contracts/NutsPlatform.sol
+++ contracts/NutsPlatform.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.5.0;
 
+import "./UnifiedStorage.sol";
+import "./InstrumentRegistry.sol";
+import "./NutsToken.sol";
+import "./NutsEscrow.sol";
+import "./IInstrument.sol";
+
 contract NutsPlatform {
-    
+    uint256 private lastIssuanceId = 0;
+    UnifiedStorage private _storage;
+    InstrumentRegistry private _instrumentRegistry;
+    NutsToken private _token;
+    NutsEscrow private _escrow;
+    uint256 constant TOKEN_AMOUNT = 10;
+
+    constructor() public {
+        _storage = new UnifiedStorage();
+        _instrumentRegistry = new InstrumentRegistry();
+        _token = new NutsToken();
+        _escrow = new NutsEscrow();
+    }
+
+    function createInstrument(address instrument_address, uint256 expiration) public {
+        _token.transferFrom(msg.sender, address(this), TOKEN_AMOUNT);
+        _instrumentRegistry.create(msg.sender, instrument_address, expiration);
+    }
+
+    function deactivateInstrument(address instrument_address) public {
+        _token.transfer(msg.sender, TOKEN_AMOUNT);
+        _instrumentRegistry.deactivate(instrument_address);
+    }
 }
