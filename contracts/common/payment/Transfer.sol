ae8ef8d243c7028dab016c82ff3149eb0fb68915
daphnedalala <daphnedalala@daphnedalaladeMacBook-Pro.local>
2019-04-22 18:00:19 -0700

    Transfer test cases

--- contracts/common/payment/Transfer.sol
+++ contracts/common/payment/Transfer.sol
@@ -6,9 +6,15 @@ import "../seriality/SizeOf.sol";
 import "../util/StringUtil.sol";
 
 /**
- * Core library to represent a balance transfer action
+ * @title Core library to represent a balance transfer action
+ * Note: One key difference between Balance and Transfer is that,
+ * Balance has at most one entry per token, but Transfer can have multiple
+ * entries as the receivers might be different!
  */
 library Transfer {
+    /**
+     * @dev Represent a single transfer action
+     */
     struct TransferAction {
         // Whether it's the Ether balance entry
         bool isEther;
@@ -19,12 +25,15 @@ library Transfer {
         uint amount;
     }
 
+    /**
+     * @dev Represents a set of transfer actions
+     */
     struct Transfers {
         TransferAction[] actions;
     }
 
     /**
-     * Serialize the transfers into bytes
+     * @dev Serialize the transfers into bytes
      */
     function save(Transfers storage transfers) internal view returns (bytes memory data) {
         // Size of one balance = size of token address(20) + + size of receiver address(20) + size of uint(32) = 84
@@ -35,9 +44,9 @@ library Transfer {
         offset -= 32;
 
         for (uint i = 0; i < transfers.actions.length; i++) {
-            TypesToBytes.stringToBytes(offset, StringUtil.addressToBytes(transfers.actions[i].tokenAddress), data);
+            TypesToBytes.addressToBytes(offset, transfers.actions[i].tokenAddress, data);
             offset -= 20;
-            TypesToBytes.stringToBytes(offset, StringUtil.addressToBytes(transfers.actions[i].receiverAddress), data);
+            TypesToBytes.addressToBytes(offset, transfers.actions[i].receiverAddress, data);
             offset -= 20;
             TypesToBytes.uintToBytes(offset, transfers.actions[i].amount, data);
             offset -= 32;
@@ -45,7 +54,7 @@ library Transfer {
     }
 
     /**
-     * Deserialize the transfers from bytes
+     * @dev Deserialize the transfers from bytes
      */
     function load(Transfers storage transfers, bytes memory data) internal {
         transfers.actions.length = 0;
@@ -54,14 +63,10 @@ library Transfer {
         offset -= 32;
         
         for (uint i = 0; i < length; i++) {
-            bytes memory addressBuffer = new bytes(BytesToTypes.getStringSize(offset, data));
-            BytesToTypes.bytesToString(offset, data, addressBuffer);
-            offset -= BytesToTypes.getStringSize(offset, data);
-            address tokenAddress = StringUtil.bytesToAddress(addressBuffer);
-            addressBuffer = new bytes(BytesToTypes.getStringSize(offset, data));
-            BytesToTypes.bytesToString(offset, data, addressBuffer);
-            offset -= BytesToTypes.getStringSize(offset, data);
-            address receiverAddress = StringUtil.bytesToAddress(addressBuffer);
+            address tokenAddress = BytesToTypes.bytesToAddress(offset, data);
+            offset -= 20;
+            address receiverAddress = BytesToTypes.bytesToAddress(offset, data);
+            offset -= 20;
             uint amount = BytesToTypes.bytesToUint256(offset, data);
             offset -= 32;
 
@@ -73,11 +78,33 @@ library Transfer {
         transfers.actions.length = 0;
     }
 
+    /**
+     * @dev Add a new Ether transfer action.
+     */
     function addEtherTransfer(Transfers storage transfers, address receiverAddress, uint amount) internal {
         transfers.actions.push(TransferAction(true, address(0x0), receiverAddress, amount));
     }
 
+    function getEtherTransfer(Transfers storage transfers, address receiverAddress) internal view returns (uint amount) {
+        for (uint i = 0; i < transfers.actions.length; i++) {
+            if (transfers.actions[i].isEther && transfers.actions[i].receiverAddress == receiverAddress) {
+                amount = transfers.actions[i].amount;
+                break;
+            }
+        }
+    }
+
     function addTokenTransfer(Transfers storage transfers, address tokenAddress, address receiverAddress, uint amount) internal {
         transfers.actions.push(TransferAction(false, tokenAddress, receiverAddress, amount));
+    }
+
+    function getTokenTransfer(Transfers storage transfers, address tokenAddress) internal view returns (address receiverAddress, uint amount) {
+        for (uint i = 0; i < transfers.actions.length; i++) {
+            if (!transfers.actions[i].isEther && transfers.actions[i].tokenAddress == tokenAddress) {
+                receiverAddress = transfers.actions[i].receiverAddress;
+                amount = transfers.actions[i].amount;
+                break;
+            }
+        }
     }
 }
---
+++ test/TestTransfer.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "../contracts/common/payment/Transfer.sol";
+
+contract TestTransfer {
+    using Transfer for Transfer.Transfers;
+
+    Transfer.Transfers private _transfers;
+    Transfer.Transfers private _transfers2;
+
+    function testShouldSetAndGetTransfers() public {
+        _transfers.clear();
+        _transfers.addEtherTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b, 100);
+        _transfers.addEtherTransfer(0x655d3828c419606e673a33cDda87CA2848f031f0, 300);
+        _transfers.addTokenTransfer(0x5b76c94Af501cDEB14338e245af26c7b551AcE29, 0x655d3828c419606e673a33cDda87CA2848f031f0, 500);
+        _transfers.addTokenTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b, 0x5b76c94Af501cDEB14338e245af26c7b551AcE29, 700);
+
+        Assert.equal(_transfers.getEtherTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b), 100, "Transfer amount should be the same");
+        Assert.equal(_transfers.getEtherTransfer(0x655d3828c419606e673a33cDda87CA2848f031f0), 300, "Transfer amount should be the same");
+
+        (address r1, uint a1) = _transfers.getTokenTransfer(0x5b76c94Af501cDEB14338e245af26c7b551AcE29);
+        Assert.equal(r1, 0x655d3828c419606e673a33cDda87CA2848f031f0, "Transfer address should be the same");
+        Assert.equal(a1, 500, "Transfer amount should be the same.");
+        (address r2, uint a2) = _transfers.getTokenTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b);
+        Assert.equal(r2, 0x5b76c94Af501cDEB14338e245af26c7b551AcE29, "Transfer address should be the same");
+        Assert.equal(a2, 700, "Transfer amount should be the same.");
+    }
+
+    function testShouldSaveAndLoadTransfers() public {
+        _transfers.clear();
+        _transfers2.clear();
+        _transfers.addEtherTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b, 100);
+        _transfers.addEtherTransfer(0x655d3828c419606e673a33cDda87CA2848f031f0, 300);
+        _transfers.addTokenTransfer(0x5b76c94Af501cDEB14338e245af26c7b551AcE29, 0x655d3828c419606e673a33cDda87CA2848f031f0, 500);
+        _transfers.addTokenTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b, 0x5b76c94Af501cDEB14338e245af26c7b551AcE29, 700);
+
+        _transfers2.load(_transfers.save());
+        Assert.equal(_transfers2.getEtherTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b), 100, "Transfer amount should be the same");
+        Assert.equal(_transfers2.getEtherTransfer(0x655d3828c419606e673a33cDda87CA2848f031f0), 300, "Transfer amount should be the same");
+
+        (address r1, uint a1) = _transfers2.getTokenTransfer(0x5b76c94Af501cDEB14338e245af26c7b551AcE29);
+        Assert.equal(r1, 0x655d3828c419606e673a33cDda87CA2848f031f0, "Transfer address should be the same");
+        Assert.equal(a1, 500, "Transfer amount should be the same.");
+        (address r2, uint a2) = _transfers2.getTokenTransfer(0x31b2D5618d36A85E4B5714fE4b4a1aE08d6ca27b);
+        Assert.equal(r2, 0x5b76c94Af501cDEB14338e245af26c7b551AcE29, "Transfer address should be the same");
+        Assert.equal(a2, 700, "Transfer amount should be the same.");
+    }
+}
