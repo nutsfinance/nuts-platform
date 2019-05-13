bdcd2450cee464fce1bf64ac612091835183d7b8
Shengda <dingshengda@gmail.com>
2019-05-13 13:10:06 -0700

    loan test case 3

--- test/integration/LoanContract.test.js
+++ test/integration/LoanContract.test.js
@@ -7,6 +7,17 @@ const NutsToken = artifacts.require("../../contracts/NutsToken.sol");
 const Loan = artifacts.require("../../contracts/instrument/Loan.sol");
 const ERC20Mintable = artifacts.require("../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol");
 
+const getEventTimestamp = async function(txHash, emitter, eventName) {
+    const receipt = await web3.eth.getTransactionReceipt(txHash);
+    const logs = emitter.decodeLogs(receipt.logs);
+    // console.log(logs);
+    const event = logs.find(e => e.event == "EventScheduled" && e.args.eventName == eventName);
+    // console.log(event);
+    // console.log(event.args);
+    // console.log(event.args.timestamp);
+    return logs.find(e => e.event == "EventScheduled" && e.args.eventName == eventName).args.timestamp.toNumber();
+}
+
 contract("NutsPlatform", ([owner, fsp, seller, buyer, tokenOwner]) => {
     before("deploy new NUTS platform and loan contracts", async function() {
         this.nutsPlatform = await NutsPlatform.deployed();
@@ -56,7 +67,7 @@ contract("NutsPlatform", ([owner, fsp, seller, buyer, tokenOwner]) => {
                 state: "Engageable"
             });
             expect(await this.nutsEscrow.balanceOfIssuance(issuanceId)).be.bignumber.equal('5');
-        })
+        }),
         // it("should fail to create issuance with invalid parameters", async function() {
         //     // Send a zero collateral token address
         //     await shouldFail.reverting.withMessage(this.nutsPlatform.createIssuance(this.loan.address,
@@ -160,6 +171,35 @@ contract("NutsPlatform", ([owner, fsp, seller, buyer, tokenOwner]) => {
         //         `borrow-amount=5&deposit-due-days=3&engagement-due-days=20&collateral-due-days=5&` +
         //         `tenor-days=30&interest-rate=10000`), "Grace period must be greater than 0");
 
-        // })
+        // }),
+        it("should become unfunded if deposit is overdue", async function() {
+            // Create issuance
+            let tx = await this.nutsPlatform.createIssuance(this.loan.address,
+                `collateral-token-address=${this.collateralToken.address}&collateral-amount=30&` + 
+                `borrow-amount=5&deposit-due-days=3&engagement-due-days=20&collateral-due-days=5&` +
+                `tenor-days=30&interest-rate=10000&grace-period=5`, {from: seller});
+            const issuanceId = tx.logs.find(e => e.event == "IssuanceCreated").args.issuanceId.toNumber();
+            await expectEvent.inTransaction(tx.receipt.transactionHash, Loan, "IssuanceStateUpdated", {
+                issuanceId: new BN(issuanceId),
+                state: "Initiated"
+            });
+            const timestamp = await getEventTimestamp(tx.receipt.transactionHash, Loan, "deposit_expired");
+            // console.log(timestamp);
+            // console.log((await time.latest()).toNumber());
+
+            // Seller deposit insufficient Ether: borrow amount = 5
+            tx = await this.nutsPlatform.deposit(issuanceId, 2, {from: seller});
+            await shouldFail.reverting.withMessage(this.nutsPlatform.processScheduledEvent(issuanceId, timestamp, "deposit_expired", "")
+                , "The scheduled event is not due now.");
+
+            // Move the timestamp
+            await time.increase(5 * 24 * 3600 + 100);
+            // Scheduled event: deposit_expired
+            tx = await this.nutsPlatform.processScheduledEvent(issuanceId, timestamp, "deposit_expired", "");
+            await expectEvent.inTransaction(tx.receipt.transactionHash, Loan, "IssuanceStateUpdated", {
+                issuanceId: new BN(issuanceId),
+                state: "Unfunded"
+            });
+        })
     })
 });
